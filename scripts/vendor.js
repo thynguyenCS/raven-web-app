825d5c585cd0bf746f64a2ae6136389f2c8fa639
tchu1997 <tuong.chu@sjsu.edu>
2020-04-12 05:48:20 -0700

    updated vendor2.html

---
+++ scripts/vendor.js
@@ -0,0 +1,13 @@
+// const viewButton = document.getElementById('view-button');
+// const tileExpand = document.getElementById('tile-expand');
+// const tile = document.getElementById('tile');
+// viewButton.addEventListener('click', (e) => {
+//     if (tileExpand.style.display=="none"){
+//         tileExpand.style.display="block";
+//         tile.classList.toggle("expanded");
+//     }else {
+//         tileExpand.style.display="none";
+//         tileExpand.classList.toggle("expanded");
+//     }
+    
+// })
