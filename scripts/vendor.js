2a8ae17faa7f5d081969f4618cd4180f218a7f2b
tchu1997 <tuong.chu@sjsu.edu>
2020-04-16 06:36:06 -0700

    vendor3.html latest

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -127,10 +127,11 @@ const logoutButton = document.getElementById('logout-button');
 logoutButton.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut();
-    success("Logout successfully");
-    window.setTimeout(function(){
-    // Move to html.index after 2 seconds
-        window.location.href = "index2.html";
-    }, 2000);
+    // success("Logout successfully");
+    // window.setTimeout(function(){
+    // // Move to html.index after 2 seconds
+    //     window.location.href = "index2.html";
+    // }, 2000);
+    window.location.href = "index2.html";
 });
 
