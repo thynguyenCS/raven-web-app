1ef1ef4c74d581a8b2bbfb02b7156f2a93aaade5
tchu1997 <tuong.chu@sjsu.edu>
2020-04-17 06:47:27 -0700

    pagination.js works only prev and next button

---
+++ scripts/pagination.js
@@ -0,0 +1,160 @@
+var current_page = 1;
+var records_per_page = 10;
+var records_per_row = 2;
+class Vendor {
+    constructor(name, location, category, logo, rating){
+        this.name = name;
+        this.location = location;
+        this.category = category;
+        this.logo = logo;
+        this.rating = rating;
+    }
+}
+
+ // Firestore data converter
+vendorConverter = {
+    toFirestore: function(vendor) {
+        return {
+            name: vendor.name,
+            location: vendor.location,
+            category: vendor.category,
+            logo: vendor.logo,
+            rating: vendor.rating
+            }
+    },
+    fromFirestore: function(snapshot, options){
+        const data = snapshot.data(options);
+        return new Vendor(data.name, data.location, data.category, data.logo, data.rating);
+    }
+}
+
+function prevPage(){
+    if (vendors.length > 0) {
+        if (current_page > 1){
+            current_page--;
+            changePage(current_page);
+        }
+    } else {
+        setTimeout(arguments.callee, 50);
+    }
+    
+}
+function nextPage(){
+    console.log(vendors);
+    if (vendors.length ==0 ){
+        window.setTimeout(nextPage,100);
+    }
+    else{
+        if (current_page < numPages()){
+            current_page++;
+            changePage(current_page);
+        }
+    }
+
+}
+var vendors = [];
+function numPages(){
+    return Math.ceil(vendors.length / records_per_page);
+}
+
+
+db.collection("vendors")
+    .withConverter(vendorConverter)
+    .orderBy('name').get().then(function(querySnapshot) {
+        
+        querySnapshot.forEach(function(doc){
+            vendor = doc.data();
+            // console.log(doc.data());
+            vendors.push(vendor);
+            // vendors.push(new Vendor(doc.name, doc.location, doc.category, doc.logo, doc.rating));
+        });
+        console.log(vendors.length);
+    }).catch(function(error){
+        console.log("Error getting documents: ", error);
+    });
+
+function getVendors() {
+    const fbVendors = [];
+    return new Promise((resolve, reject) => {
+        db.collection("vendors")
+        .withConverter(vendorConverter)
+        .orderBy("name")
+        .get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+            vendor = doc.data();
+            fbVendors.push(vendor);
+            });
+    
+            resolve(fbVendors);
+        })
+        .catch(function (error) {
+            reject(error);
+        });
+    });
+    }
+
+function changePage(page){
+    var btn_next = document.getElementById('next-button');
+    var btn_prev = document.getElementById('prev-button');
+    var listing_table = document.getElementById('default-list');
+   
+    //validate page
+    if (page < 1) page=1;
+    if (page > numPages()) page = numPages();
+    console.log(current_page);
+    listing_table.innerHTML = "";
+    let html ="";
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++){
+        let vendorCard =""; 
+        if (vendors[i]){
+            vendorCard = 
+                        `<div class="col-6">
+                            <div class="tile u-no-padding">
+                                <div class="row w-100 u-no-padding">
+                                    <div class="tile__icon">
+                                        <figure class="avatar">
+                                            <img class="h-100" src=${vendors[i].logo}>
+                                        </figure>
+                                    </div>
+                                    <div class="tile__container tile-padded"> 
+                                            <h6 class="tile__title u-no-margin">${vendors[i].name}</b></h6>
+                                            <p class="tile__subtitle">${vendors[i].location}</p>
+                                            <span class="info">${vendors[i].category}</span> 
+                                    </div>
+                                    
+                                    <div class="tile__container u-text-right tile-padded">
+                                        <span class="fa fa-star checked yellow"></span>
+                                        <span class="fa fa-star checked yellow"></span>
+                                        <span class="fa fa-star checked yellow"></span>
+                                        <span class="fa fa-star checked yellow"></span>
+                                        <span class="far fa-star yellow"></span>
+                                        <span class="info light-blue">5 reviews</span>
+                                    </div>
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>`;
+        }
+        
+        if ((i % 2) == 0){
+            html += '<div class="row">' + vendorCard;
+           
+        }
+        if( (i % 2) > 0){
+            html += vendorCard +'</div><space class="large"></space>';
+        }
+
+    }
+    listing_table.innerHTML += html;
+    
+}
+
+window.onload = function() {
+    getVendors()
+    .then( fbVendors => { 
+       vendors = fbVendors;
+       changePage(1);
+    })
+};
--- scripts/vendor.js
+++ scripts/vendor.js
@@ -67,65 +67,66 @@ searchButton.addEventListener('click', (e)=>{
     })
  
 /******DISPLAY VENDOR *******/
-function createVendorCard(change, i){
-    let html = '';  
-        const vendor = change.doc.data()
-        let startRow = `<div class="row">` 
-        let endRow = `</div>
-        <space class="large"></space>`
-        let content = ` <div class="col-5">
-        <div class="tile u-no-padding">
-            <div class="row u-no-padding">
-                <!-- vendor pic -->
-                <div class="tile__icon">
-                    <figure class="avatar">
-                        <img src=${vendor.logo}>
-                    </figure>
-                </div>
-                <!-- Vendor's name, location, industry -->
-                <div class="tile__container tile-padded"> 
-                        <h6 class="tile__title u-no-margin">${vendor.name}</b></h6>
+// THIS PART IS MOVED TO PAGINATION.JS
+// function createVendorCard(change, i){
+//     let html = '';  
+//         const vendor = change.doc.data()
+//         let startRow = `<div class="row">` 
+//         let endRow = `</div>
+//         <space class="large"></space>`
+//         let content = ` <div class="col-6">
+//         <div class="tile u-no-padding">
+//             <div class="row w-100 u-no-padding">
+//                 <!-- vendor pic -->
+//                 <div class="tile__icon">
+//                     <figure class="avatar">
+//                         <img class="h-100" src=${vendor.logo}>
+//                     </figure>
+//                 </div>
+//                 <!-- Vendor's name, location, industry -->
+//                 <div class="tile__container tile-padded"> 
+//                         <h6 class="tile__title u-no-margin">${vendor.name}</b></h6>
 
-                        <p class="tile__subtitle">${vendor.location}</p>
+//                         <p class="tile__subtitle">${vendor.location}</p>
                        
-                        <span class="info">Category: ${vendor.category}</span> 
-                </div>
-                <!-- Vendor's rating stars -->
-                <div class="tile__container u-text-right tile-padded">
-                    <span class="fa fa-star checked yellow"></span>
-                    <span class="fa fa-star checked yellow"></span>
-                    <span class="fa fa-star checked yellow"></span>
-                    <span class="fa fa-star checked yellow"></span>
-                    <span class="far fa-star yellow"></span>
-                    <span class="info light-blue">5 reviews</span>
-                </div>
-            </div>            
-        </div>
-    </div>`
-        //if changes.length == 1
-        // if (size == 0){
-        //     console.log('Not found')
-        // }
-        // if(i%2==0 && i == size - 1)
-        //     html += startRow + content + endRow;
-        if(i%2==0)
-            html += startRow + content;
-        else if (i%2 ==1)
-            html += content + endRow;
-        //i++;
-    return html
-}
+//                         <span class="info">Category: ${vendor.category}</span> 
+//                 </div>
+//                 <!-- Vendor's rating stars -->
+//                 <div class="tile__container u-text-right tile-padded">
+//                     <span class="fa fa-star checked yellow"></span>
+//                     <span class="fa fa-star checked yellow"></span>
+//                     <span class="fa fa-star checked yellow"></span>
+//                     <span class="fa fa-star checked yellow"></span>
+//                     <span class="far fa-star yellow"></span>
+//                     <span class="info light-blue">5 reviews</span>
+//                 </div>
+//             </div>            
+//         </div>
+//     </div>`
+//         //if changes.length == 1
+//         // if (size == 0){
+//         //     console.log('Not found')
+//         // }
+//         // if(i%2==0 && i == size - 1)
+//         //     html += startRow + content + endRow;
+//         if(i%2==0)
+//             html += startRow + content;
+//         else if (i%2 ==1)
+//             html += content + endRow;
+//         //i++;
+//     return html
+// }
 
-db.collection('vendors').orderBy('name').get().then(snap=>{
-    let html = ''
-    let defaultData = snap.docChanges();
-    var i = 0       
-    defaultData.forEach(change=>{
-        html += createVendorCard(change,i)
-        i++
-    })
-    defaultList.innerHTML = html
-})
+// db.collection('vendors').orderBy('name').get().then(snap=>{
+//     let html = ''
+//     let defaultData = snap.docChanges();
+//     var i = 0       
+//     defaultData.forEach(change=>{
+//         html += createVendorCard(change,i)
+//         i++
+//     })
+//     defaultList.innerHTML = html
+// })
 
 
 /******LOG OUT ********/
