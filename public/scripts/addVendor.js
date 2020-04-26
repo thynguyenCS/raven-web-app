ce360680896f74829391a0725d07bd893dd6db75
toandaosjsu <toan.dao@sjsu.edu>
2020-04-25 23:31:41 -0700

    add vendor

---
+++ public/scripts/addVendor.js
@@ -0,0 +1,9 @@
+/******LOG OUT ********/
+const logoutButton = document.getElementById('logout-button');
+logoutButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+    window.location.href = "index2.html";
+});
+
+/******ADD VENDOR ********/
