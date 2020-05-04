315e5101e0add3016e9e0a8eb89868f8f97bfa4e
toandaosjsu <toan.dao@sjsu.edu>
2020-05-03 21:40:26 -0700

    add auth.user

--- public/scripts/addVendor.js
+++ public/scripts/addVendor.js
@@ -37,69 +37,78 @@ const addVendorMsg = document.getElementById('vendor-added');
 
   // Once all inputs are valid, register user credential to firebase
   addVendorForm.addEventListener('submit', (e)=>{
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        
+          // where is the part of checking for logged-in user??
+          e.preventDefault(); // prevent form from submitting
 
-    // where is the part of checking for logged-in user??
-    e.preventDefault(); // prevent form from submitting
+          console.log(input.value); // get tags and print them
+          
+          if (input.value == ''){
+            displayToast("Please select at least one keyword.");
+            setTimeout(() => {
+              closeToast();
+            },3000);
+          } else {
 
-    console.log(input.value); // get tags and print them
-    
-    if (input.value == ''){
-      displayToast("Please select at least one keyword.");
-      setTimeout(() => {
-        closeToast();
-      },3000);
-    } else {
+            // var string = signupForm.cat.value
+            // var arr = string.split(', '); // split string on comma space
+            var street = addVendorForm.street.value
+            var city = addVendorForm.city.value
+            var state = addVendorForm.state.value
+            var zip = addVendorForm.zip.value
+            var loc = street + ', ' + city + ', ' + state + ', ' + zip
+            // let arr = Array.from(vendorTags)
+            db.collection('newVendor').add({
+                name: addVendorForm.name.value,
+                // street: signupForm.street.value,
+                // category: arr, // signupForm.cat.value,
+                // category: arr,
+                logo: "img/ravenCircle.png",
+                rating: 0,
+                comment: "New Vendor - Waiting for confirm",
+                location: loc
+                // city: signupForm.city.value,
+                // country: signupForm.country.value,
+                // zip: signupForm.zip.value
 
-      // var string = signupForm.cat.value
-      // var arr = string.split(', '); // split string on comma space
-      var street = addVendorForm.street.value
-      var city = addVendorForm.city.value
-      var state = addVendorForm.state.value
-      var zip = addVendorForm.zip.value
-      var loc = street + ', ' + city + ', ' + state + ', ' + zip
-      // let arr = Array.from(vendorTags)
-      db.collection('newVendor').add({
-          name: addVendorForm.name.value,
-          // street: signupForm.street.value,
-          // category: arr, // signupForm.cat.value,
-          // category: arr,
-          logo: "img/ravenCircle.png",
-          rating: 0,
-          comment: "New Vendor - Waiting for confirm",
-          location: loc
-          // city: signupForm.city.value,
-          // country: signupForm.country.value,
-          // zip: signupForm.zip.value
+              }).then(() => {
+                //reset form
+                addVendorForm.reset();
+                e.preventDefault();
 
-        }).then(() => {
-          //reset form
-          addVendorForm.reset();
-          e.preventDefault();
+                // display confirm message
+                addVendorMsg.style.display = "block";
+                addVendorForm.style.display="none";
 
-          // display confirm message
-          addVendorMsg.style.display = "block";
-          addVendorForm.style.display="none";
-
-          window.setTimeout(function(){
-            // Move to html.index after 2 seconds
-            window.location.href = "vendor3.html";
-          }, 2000);
-        }).catch(err => {
-          console.log(err.message);
-        });
-    }
+                window.setTimeout(function(){
+                  // Move to html.index after 2 seconds
+                  window.location.href = "vendor3.html";
+                }, 2000);
+              }).catch(err => {
+                console.log(err.message);
+              });
+          }
+      } else {
+              // No user is signed in.
+              e.preventDefault();
+              window.location.href = "index.html";
+      }
     });
 
-    function success(msg){
-      Swal.fire({
-          icon:'success',
-          title:'<p style="color:white">' + msg + '</p>',
-          width: 420,
-          showConfirmButton: false,
-          background:'#755ee8'
-        })
+  });
+
+function success(msg){
+  Swal.fire({
+    icon:'success',
+    title:'<p style="color:white">' + msg + '</p>',
+    width: 420,
+    showConfirmButton: false,
+    background:'#755ee8'
+  })
   
-  }
+}
 /******VIEW PROFILE ********/
 const profileButton = document.getElementById('profile');
 profileButton.addEventListener('click', (e) => {
